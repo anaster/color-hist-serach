f9b50f7aa58536345664bdcfb0694c3a43afdeb8
anaster <tavrenard@gmail.com>
2020-06-03 21:12:32 +0300

    Finally, getting things done.

---
+++ rgbhist.py
@@ -0,0 +1,17 @@
+import cv2
+import imutils
+
+
+class RGBHist:
+    def __init__(self, bins):
+        self.bins = bins
+
+    def describe(self, image):
+        hist = cv2.calcHist([image], [0, 1, 2], None, self.bins, [0, 256, 0, 256, 0, 256])
+
+        if imutils.is_cv2():
+            hist = cv2.normalize(hist)
+        else:
+            hist = cv2.normalize(hist, hist)
+        return hist.flatten()
+
