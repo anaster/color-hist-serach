8fed6f675e8564a2711f2da239b24565cb73ade4
anaster <tavrenard@gmail.com>
2020-06-11 02:46:02 +0300

    Added good dataset

---
+++ start.bat
@@ -0,0 +1,4 @@
+python search.py --dataset images --index index.cpickle --query queries/blue_words.jpg
+python search.py --dataset images --index index.cpickle --query queries/green_apple.jpg
+python search.py --dataset images --index index.cpickle --query queries/red_car.jpg
+python search.py --dataset images --index index.cpickle --query queries/yellow_cat.jpg
